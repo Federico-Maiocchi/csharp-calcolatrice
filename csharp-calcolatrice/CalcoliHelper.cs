161d98782644f9321df098076f72298cdc781f38
Federico-Maiocchi <maiocchifederico@gmail.com>
2024-04-23 15:18:01 +0200

    Aggiungere i file di progetto.

---
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace csharp_calcolatrice
+{
+    //Creare una classe di helper CalcoliHelper.
+    //Come visto a lezione, strutturare la classe in modo che non possa essere istanziata e che presenti i seguenti metodi `static`:
+    internal static class CalcoliHelper
+    {
+        //- Somma di due numeri interi
+        public static int SumInt(int a, int b)
+        {
+            int totSumInt = a + b;
+
+            return totSumInt;
+        }
+        
+        //- Somma di due numeri double
+        public static double SumDouble(double a, double b)
+        {
+            double totSumDouble = Math.Round((a + b), 2);
+
+            return totSumDouble;
+        }
+
+        //- Differenza tra due numeri interi
+        public static int SubInt(int a, int b)
+        {
+            int totSubInt = a - b;
+
+            return totSubInt;
+        }
+
+        //- Differenza tra due numeri double
+        public static double SubDouble(double a, double b)
+        {
+            double totSubDouble = Math.Round((a - b), 2);
+
+            return totSubDouble;
+        }
+
+        //- Moltiplicazione di due numeri interi
+        public static int MultiplicationInt( int a, int b)
+        {
+            int totMultiplicationInt = a * b;
+
+            return totMultiplicationInt;
+        }
+
+        //- Moltiplicazione di due numeri double
+        public static double MultiplicationDouble(double a, double b)
+        {
+            double totMultiplicationDouble = Math.Round((a * b), 2);
+
+            return totMultiplicationDouble;
+        }
+
+        //- Valore assoluto di un numero intero
+        public static int AbsoluteValueInt(int a)
+        {
+            if (a < 0)
+            {
+                return -a;
+            } 
+            else
+            {
+                return a;
+            }
+        }
+
+        //- Valore assoluto di un numero double
+        public static double AbsoluteValueDouble(double a)
+        {
+            if (a < 0)
+            {
+                return - a;
+            }
+            else
+            {
+                return a;
+            }
+        }
+
+        //- Minimo tra due numeri interi
+        public static int MinimumInt(int a, int b)
+        {
+            if (a < b)
+            {
+                return a;
+            }
+            else 
+            {
+                return b;
+            }
+        }
+
+        //- Minimo tra due numeri double
+        public static double MiniumDouble(double a, double b)
+        {
+            if ( a < b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+        //- Massimo tra due numeri interi
+        public static int MaximunInt(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+        //- Massimo tra due numeri double
+        public static double MaximunDouble(double a, double b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+        //Il fatto di dover scrivere lo stesso metodo per tipi di parametro diversi applica nella pratica uno dei principi di programmazione ad oggetti che abbiamo visto oggi…Quale? Scrivetelo in un commento all’interno del codice! :faccia_nerd:
+       
+        //**BONUS**:
+        //Aggiungete il metodo per l’elevamento a potenza che prende come parametri due numeri interi(base ed esponente) e ritorna la base elevata all’esponente.Occhio che sia la base che l’esponente potrebbero essere uguali a zero oppure numeri negativi!
+        //(caso esponente = 0 e base = 0 anche se in matematica sarebbe un numero indefinito usiamo l’approccio informatico che lo calcola come 1)
+    }
+}
---
+++ csharp-calcolatrice/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace csharp_calcolatrice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Testate tutti i metodi della vostra classe di helper(con un esempio per ogni casistica).
+            int a = 5;
+            int b = 10;
+
+            Console.WriteLine($"Somma di due numeri interi: {CalcoliHelper.SumInt(a, b)}");
+            Console.WriteLine($"Differenza tra due numeri interi: {CalcoliHelper.SubInt(a, b)}");
+            Console.WriteLine($"Moltiplicazione di due numeri interi: {CalcoliHelper.MultiplicationInt(a, b)}");
+            Console.WriteLine($"Valore assoluto di un numero intero: {CalcoliHelper.AbsoluteValueInt(a)}");
+            Console.WriteLine($"Minimo tra due numeri interi: {CalcoliHelper.MinimumInt(a, b)}");
+            Console.WriteLine($"Massimo tra due numeri interi: {CalcoliHelper.MaximunInt(a, b)}");
+
+            double c = 2.4;
+            double d = 12.64;
+
+            Console.WriteLine($"Somma di due numeri double: {CalcoliHelper.SumDouble(c, d)}");
+            Console.WriteLine($"Differenza tra due numeri double: {CalcoliHelper.SubDouble(c, d)}");
+            Console.WriteLine($"Moltiplicazione di due numeri double: {CalcoliHelper.MultiplicationDouble(c, d)}");
+            Console.WriteLine($"Valore assoluto di un numero double: {CalcoliHelper.AbsoluteValueDouble(c)}");
+            Console.WriteLine($"Minimo tra due numeri double: {CalcoliHelper.MiniumDouble(c, d)}");
+            Console.WriteLine($"Massimo tra due numeri double: {CalcoliHelper.MaximunDouble(c, d)}");
+
+
+        }
+    }
+}
